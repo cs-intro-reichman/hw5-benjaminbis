e0f872b49662803e5085f044601d94a1b9b9f94a
benjaminbis <benjaminbismuth2@gmail.com>
2024-12-12 10:29:05 +0200

    Update Scrabble.java

--- Scrabble.java
+++ Scrabble.java
@@ -1,16 +1,24 @@
+/*
+ * RUNI version of the Scrabble game.
+ */
 public class Scrabble {
 
     static final String WORDS_FILE = "dictionary.txt";
-    static final int HAND_SIZE = 10;
-    static String[] DICTIONARY = new String[100000];
+    static final int[] SCRABBLE_LETTER_VALUES = { 1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3,
+                                                  1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10 };
+    static int HAND_SIZE = 10;
+    static int MAX_NUMBER_OF_WORDS = 100000;
+    static String[] DICTIONARY = new String[MAX_NUMBER_OF_WORDS];
     static int NUM_OF_WORDS;
 
     public static void init() {
         In in = new In(WORDS_FILE);
+        System.out.println("Loading word list from file...");
         NUM_OF_WORDS = 0;
         while (!in.isEmpty()) {
             DICTIONARY[NUM_OF_WORDS++] = in.readString().toLowerCase();
         }
+        System.out.println(NUM_OF_WORDS + " words loaded.");
     }
 
     public static boolean isWordInDictionary(String word) {
@@ -23,10 +31,6 @@ public class Scrabble {
     }
 
     public static int wordScore(String word) {
-        int[] SCRABBLE_LETTER_VALUES = {
-            1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3,
-            1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10
-        };
         int score = 0;
         for (int i = 0; i < word.length(); i++) {
             char letter = word.charAt(i);
@@ -55,7 +59,7 @@ public class Scrabble {
         int score = 0;
         In in = new In();
         while (hand.length() > 0) {
-            System.out.println("Current Hand: " + formatHand(hand));
+            System.out.println("Current Hand: " + hand);
             System.out.println("Enter a word, or '.' to finish playing this hand:");
             String input = in.readString();
             if (input.equals(".")) {
@@ -98,14 +102,6 @@ public class Scrabble {
         return true;
     }
 
-    public static String formatHand(String hand) {
-        StringBuilder formattedHand = new StringBuilder();
-        for (int i = 0; i < hand.length(); i++) {
-            formattedHand.append(hand.charAt(i)).append(" ");
-        }
-        return formattedHand.toString().trim();
-    }
-
     public static void playGame() {
         init();
         In in = new In();
@@ -124,7 +120,39 @@ public class Scrabble {
         }
     }
 
+    public static void testBuildingTheDictionary() {
+        init();
+        for (int i = 0; i < 5; i++) {
+            System.out.println(DICTIONARY[i]);
+        }
+        System.out.println(isWordInDictionary("mango"));
+    }
+
+    public static void testScrabbleScore() {
+        System.out.println(wordScore("bee"));
+        System.out.println(wordScore("babe"));
+        System.out.println(wordScore("friendship"));
+        System.out.println(wordScore("running"));
+    }
+
+    public static void testCreateHands() {
+        System.out.println(createHand());
+        System.out.println(createHand());
+        System.out.println(createHand());
+    }
+
+    public static void testPlayHands() {
+        init();
+        playHand("ocostrza");
+        playHand("arbffip");
+        playHand("aretiin");
+    }
+
     public static void main(String[] args) {
+        testBuildingTheDictionary();
+        testScrabbleScore();
+        testCreateHands();
+        testPlayHands();
         playGame();
     }
 }
